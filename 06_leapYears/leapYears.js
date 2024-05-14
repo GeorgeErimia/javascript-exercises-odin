22e1cb7876593a145152c7857c70e38956eab2fc
George Erimia <l3ttuceftw@gmail.com>
2024-05-14 21:25:15 +0300

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check if the year is divisible by 4
+    if(year % 4 === 0) {
+        // If the year is divisible by 4 we check if it's divisible by 100
+        if(year % 100 === 0) {
+            // If the year is divisible by 4, 100 and 400 it's a LEAP YEAR
+            if(year % 400 === 0) {
+                return true
+            }
+            // Otherwise it's not a leap year
+            else return false;
+        }
+        // If the year is divisible by 4 but not by 100, it's a LEAP YEAR 
+        else {
+            return true
+        }
+    }
+    // If the year is not divisible by 4, it's not a leap year
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
