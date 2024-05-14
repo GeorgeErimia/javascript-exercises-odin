fd122fb8197f93911285fd898af213786b4b04d9
George Erimia <l3ttuceftw@gmail.com>
2024-05-14 20:29:42 +0300

    Solve 04_removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    let length = string.length
+    while(length > 0) {
+        result = result + string.charAt(length-1)
+        length--;
+    }
+    return result;
 };
 
 // Do not edit below this line
