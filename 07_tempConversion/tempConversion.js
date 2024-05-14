2e6f871524c3be1b8c6b5879c1aad285c50eaca9
George Erimia <l3ttuceftw@gmail.com>
2024-05-14 21:32:59 +0300

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  const tempCelsius = (tempFahrenheit - 32) * (5/9)
+  if(tempCelsius % 10 == 0) {
+    return tempCelsius
+  } else return parseFloat(tempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  const tempFahrenheit = tempCelsius * (9/5) + 32
+  if(tempFahrenheit % 10 == 0) {
+    return tempFahrenheit;
+  } else return parseFloat(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
