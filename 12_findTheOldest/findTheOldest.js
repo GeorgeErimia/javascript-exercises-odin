f7a81f3375194d63bf399a18bb6e34bbc7de493e
George Erimia <l3ttuceftw@gmail.com>
2024-05-20 12:29:11 +0300

    Solved 11_getTheTitles and 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(birthYear, deathYear) {
+    if(!deathYear) {
+        deathYear = (new Date()).getFullYear();
+    }
+    return deathYear - birthYear;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
