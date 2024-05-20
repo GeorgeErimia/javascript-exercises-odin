a7c21c3dc9b718d7b3fd7b07c010fdc09998b742
George Erimia <l3ttuceftw@gmail.com>
2024-05-20 12:01:23 +0300

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    // to lower case
+    string = string.toLowerCase();
+
+    // Remove punctuation from text
+    var punctuation = /[\.,?!]/g;
+    string = string.replace(punctuation, "");
+
+    // Remove spaces from text
+    string = string.replace(/\s/g, '')
+
+    for(let i = 0; i < string.length / 2; i++) {
+        if(string.charAt(i) !== string.charAt(string.length - i - 1))
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
